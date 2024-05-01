9ae1c8efba7e481641b97425676953722e119000
paunov0000 <legendarygriefer96@gmail.com>
2024-05-01 15:16:55 +0300

    Created calculateWinner function which returns the player if winner is present and null if not

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,31 @@ export default function Board() {
 		setXIsNext(!xIsNext);
 	}
 
+	function calculateWinner(squares) {
+		const lines = [
+			[0, 1, 2],
+			[3, 4, 5],
+			[6, 7, 8],
+			[0, 3, 6],
+			[1, 4, 7],
+			[2, 5, 8],
+			[0, 4, 8],
+			[2, 4, 6],
+		];
+
+		for (let index = 0; index < lines.length; index++) {
+			const [a, b, c] = lines[index];
+			if (
+				squares[a] &&
+				squares[a] === squares[b] &&
+				squares[a] === squares[c]
+			) {
+				return squares[a];
+			}
+		}
+		return null;
+	}
+
 	return (
 		<>
 			<div className='board-row'>
