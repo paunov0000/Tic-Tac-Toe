400c1aff8decbc9d4844d9e25fb200b6071afc2d
paunov0000 <legendarygriefer96@gmail.com>
2024-05-01 14:35:02 +0300

    Formatted

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,9 +1,12 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 export default function Square({ value, onSquareClick }) {
-  return (
-    <button onClick={onSquareClick} className="square">
-      {value}
-    </button>
-  );
+	return (
+		<button
+			onClick={onSquareClick}
+			className='square'
+		>
+			{value}
+		</button>
+	);
 }
