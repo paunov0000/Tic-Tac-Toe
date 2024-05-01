fed7048d5c88fb6d1942e2ff9df309d4c3527b80
paunov0000 <legendarygriefer96@gmail.com>
2024-05-01 13:13:58 +0300

    Created a handleClick function which is passed to each Square component within an anonymous function with param its' square index to update

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,28 @@ import Square from "./components/Square";
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
+  function handleClick(i) {
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
+  }
+
   return (
     <>
       <div className="board-row">
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)}></Square>
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)}></Square>
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)}></Square>
       </div>
       <div className="board-row">
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)}></Square>
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)}></Square>
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)}></Square>
       </div>
       <div className="board-row">
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)}></Square>
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)}></Square>
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)}></Square>
       </div>
     </>
   );
