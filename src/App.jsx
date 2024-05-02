94fd5fbffbef93018e7ee4ad4b7443e9fc190c2f
paunov0000 <legendarygriefer96@gmail.com>
2024-05-02 14:04:26 +0300

    created the game grid

--- src/App.jsx
+++ src/App.jsx
@@ -56,42 +56,51 @@ export default function Board() {
 				<Square
 					value={squares[0]}
 					onSquareClick={() => handleClick(0)}
+					className='border-b-2 border-r-2 border-black w-8 h-8'
 				></Square>
 				<Square
 					value={squares[1]}
 					onSquareClick={() => handleClick(1)}
+					className='border-b-2 border-r-2 border-black w-8 h-8'
 				></Square>
 				<Square
 					value={squares[2]}
 					onSquareClick={() => handleClick(2)}
+					className='border-b-2 border-black w-8 h-8'
 				></Square>
 			</div>
 			<div className='board-row'>
 				<Square
 					value={squares[3]}
 					onSquareClick={() => handleClick(3)}
+					className='border-b-2 border-r-2 border-black w-8 h-8'
 				></Square>
 				<Square
 					value={squares[4]}
 					onSquareClick={() => handleClick(4)}
+					className='border-b-2 border-r-2 border-black w-8 h-8'
 				></Square>
 				<Square
 					value={squares[5]}
 					onSquareClick={() => handleClick(5)}
+					className='border-b-2 border-black w-8 h-8'
 				></Square>
 			</div>
 			<div className='board-row'>
 				<Square
 					value={squares[6]}
 					onSquareClick={() => handleClick(6)}
+					className='border-r-2 border-black w-8 h-8'
 				></Square>
 				<Square
 					value={squares[7]}
 					onSquareClick={() => handleClick(7)}
+					className='border-r-2 border-black w-8 h-8'
 				></Square>
 				<Square
 					value={squares[8]}
 					onSquareClick={() => handleClick(8)}
+					className='w-8 h-8'
 				></Square>
 			</div>
 		</>
