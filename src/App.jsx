ef077ab5041221ad78cd17e822444387b649d0c6
paunov0000 <legendarygriefer96@gmail.com>
2024-05-30 23:29:53 +0300

    updated currentStage to be the element of history on currentMove index, instead of the last index of history array

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ export default function Game() {
 	const [xIsNext, setXIsNext] = useState(true);
 	const [history, setHistory] = useState([Array(9).fill(null)]);
 	const [currentMove, setCurrentMove] = useState(0);
+	const currentStage = history[currentMove];
 
 	function handlePlay(nextSquares) {
 		const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
