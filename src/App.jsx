13bdfd5eb8fbd28acd29a54ab8a7cf3a269c7e30
paunov0000 <legendarygriefer96@gmail.com>
2024-05-30 23:13:31 +0300

    Added a currentMove state

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Board from './components/Board';
 export default function Game() {
 	const [xIsNext, setXIsNext] = useState(true);
 	const [history, setHistory] = useState([Array(9).fill(null)]);
-	const currentStage = history[history.length - 1];
+	const [currentMove, setCurrentMove] = useState(0);
 
 	function handlePlay(nextSquares) {
 		setHistory([...history, nextSquares]);
