7141e594d443d05339a74879d087f1914fa49fb7
paunov0000 <legendarygriefer96@gmail.com>
2024-05-30 23:28:54 +0300

    Created a nextHistory variable which makes a copy of history up until currentMove + 1 and appends nextSquares. History is being set to the nextHistory variable and currentMove is set to the last index of nextHistory

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,9 @@ export default function Game() {
 	const [currentMove, setCurrentMove] = useState(0);
 
 	function handlePlay(nextSquares) {
-		setHistory([...history, nextSquares]);
+		const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+		setHistory(nextHistory);
+		setCurrentMove(nextHistory.length - 1);
 		setXIsNext(!xIsNext);
 	}
 
